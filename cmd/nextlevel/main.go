dfc94aa3f5082121599fe92ad7fc775c72300403
QOZU <64510789+QOZU@users.noreply.github.com>
2025-08-05 21:00:33 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/nextlevel/main.go
@@ -0,0 +1,23 @@
+// cmd/nextlevel/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"nextlevel/internal/nextlevel"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := nextlevel.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
